3d0cac254f9db23674a9264bb35c428813f28a7e
alejandratub <alitatc@hotmail.com>
2017-08-31 07:48:43 -0500

    Update mainArbol.cpp

--- mainArbol.cpp
+++ mainArbol.cpp
@@ -5,19 +5,18 @@ using namespace std;
 
 int main()
 {
-	ArbolBinario p1;
+	ArbolBinario arbol;
 	string nombre;
 	cout << " Ingresa el nombre del archivo que desea abrir (Ej. archivo.txt): ";
-    cin >> nombre;
+    	cin >> nombre;
 	
 	clock_t t;
-      int f;
-      t = clock();
-      p1.crear(nombre);
-    t = clock() - t;
-    printf ("%f seconds\n",((float)t)/CLOCKS_PER_SEC);
-	//p1.inorden(p1.raiz);
-	//cout<<endl;
+        int f;
+        t = clock();
+        arbol.crear(nombre);
+    	t = clock() - t;
+   	 printf ("%f seconds\n",((float)t)/CLOCKS_PER_SEC);
+
 	
 	return 0;
 }
